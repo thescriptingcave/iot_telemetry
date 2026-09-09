0544f0c4dc23dc4f598cb7b8ea2a6da1229efb30
Robert Harding <rh.jr@outlook.com>
2026-09-09 11:52:34 -0700

    Add Superset Celery worker and Redis-based async query support

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,5 +1,7 @@
 import os
 
+from cachelib.redis import RedisCache
+
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "SQLALCHEMY_DATABASE_URI",
     "postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}".format(
@@ -10,3 +12,49 @@ SQLALCHEMY_DATABASE_URI = os.environ.get(
         db=os.environ.get("DATABASE_DB", "superset"),
     ),
 )
+
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_meta_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": 0,
+}
+
+DATA_CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_data_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": 0,
+}
+
+RESULTS_BACKEND = RedisCache(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=1,
+    key_prefix="superset_results_",
+)
+
+class CeleryConfig:
+    broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    imports = (
+        "superset.sql_lab",
+        "superset.tasks.scheduler",
+        "superset.tasks.thumbnails",
+        "superset.tasks.cache",
+        "superset.tasks.slack",
+    )
+    result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    worker_prefetch_multiplier = 1
+    task_acks_late = True
+
+
+CELERY_CONFIG = CeleryConfig
+
+SQLLAB_ASYNC = True
